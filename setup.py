b56c1ea2d9ce3b25bf7c7e38d16e34195f26fbaf
siletsokolov <siletsokolov@gmail.com>
2015-06-25 17:29:08 +0300

    fix(app): fix package data

--- setup.py
+++ setup.py
@@ -1,21 +1,41 @@
-from distutils.core import setup
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from setuptools import setup
+
+
+def get_packages(package):
+    return [dirpath for dirpath, dirnames, filenames in os.walk(package)
+            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+
+def get_package_data(package):
+    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
+            for dirpath, dirnames, filenames in os.walk(package)
+            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+    filepaths = []
+    for base, filenames in walk:
+        filepaths.extend([os.path.join(base, filename)
+                          for filename in filenames])
+    return {package: filepaths}
+
 
 setup(
     name='django-treasuremap',
-    version='0.2',
+    version='0.2.1',
     url='https://github.com/silentsokolov/django-treasuremap',
     license='MIT',
     author='Dmitriy Sokolov',
     author_email='silentsokolov@gmail.com',
     description='django-treasuremap app, makes it easy to store and display the location on the map using different providers (Google, Yandex).',
-    packages=['treasuremap'],
     zip_safe=False,
     include_package_data=True,
     platforms='any',
-    install_requires=[
-        'django>=1.6',
-    ],
-    tests_require=['Django'],
+    packages=get_packages('treasuremap'),
+    package_data=get_package_data('treasuremap'),
+    install_requires=[],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Development Status :: 5 - Production/Stable',
--- treasuremap/__init__.py
+++ treasuremap/__init__.py
@@ -6,6 +6,10 @@ from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 
 
+__author__ = 'Dmitriy Sokolov'
+__version__ = '0.2.1'
+
+
 default_app_config = 'treasuremap.apps.TreasureMapConfig'
 
 if not hasattr(settings, 'TREASURE_MAP'):
