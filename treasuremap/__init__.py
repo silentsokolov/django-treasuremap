3fa220f4c4e66fd66ee4908bde10e2d8300e066b
Dmitriy Sokolov <silentsokolov@gmail.com>
2018-10-15 17:12:14 +0300

    Prepare 0.2.7

--- treasuremap/__init__.py
+++ treasuremap/__init__.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 
 __author__ = 'Dmitriy Sokolov'
-__version__ = '0.2.6'
+__version__ = '0.2.7'
 
 
 default_app_config = 'treasuremap.apps.TreasureMapConfig'
